99566755b756790a5936935053c592e2848fbc85
rbkEsraaRamahi <isrsms7i@gmail.com>
2019-07-15 10:27:57 +0300

    build the model

---
+++ db.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("we're connected!")
+});
+
+var toDoSchema = new mongoose.Schema({
+  task: String
+});
+
+module.exports = mongoose.model('ToDo', toDoSchema);
