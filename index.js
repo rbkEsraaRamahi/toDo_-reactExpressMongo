be63d755cf8d3e89819afa1366963404346521c4
rbkEsraaRamahi <isrsms7i@gmail.com>
2019-07-15 10:30:24 +0300

    add the queries

--- index.js
+++ index.js
@@ -1,7 +1,64 @@
 const express = require('express')
 const app = express()
 const port = 5000
+var mongoose = require('mongoose');
+const ToDo = require("./db");
+ 
+// parse application/json
+app.use(express.json());
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', function(req, res){
+  ToDo.find({}, function(err, data){
+    if(err){
+        console.log(err);
+        return
+    }
+
+    if(data.length == 0) {
+        console.log("No record found")
+        return
+    }
+
+    console.log(data);
+    res.json(data)
+  })
+})
+
+app.post('/', function(req, res){
+  ToDo.create({task:req.body.task}, function(err, data){
+    if(err){
+        console.log(err);
+        return
+    }
+
+    if(data.length == 0) {
+        console.log("No record found")
+        return
+    }
+
+    console.log(data);
+    res.json(data)
+  })
+})
+
+app.put('/', function(req, res){
+  var id = req.body._id;
+  var task = req.body.task;
+  ToDo.findByIdAndUpdate(id,{$set:{task:task}},{new:true}, function(err, doc){
+    if (err) return res.send(500, { error: err });
+    return res.send("succesfully saved");
+  })
+})
+
+app.delete('/', function(req, res){
+  var id = req.body._id;
+  ToDo.findOneAndDelete(id, function(err, doc){
+    if (err) {console.log(err) 
+      return }
+    return res.json(doc);
+  })
+})
+
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
